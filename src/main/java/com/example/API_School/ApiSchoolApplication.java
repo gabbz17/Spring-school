1c235dc15284eaf779740b5e56d8526afad9f112
Gabriel Coutinho <coutinho.dev17@gmail.com>
2024-11-05 09:12:30 -0300

    Iniciando repositorio

---
+++ src/main/java/com/example/API_School/ApiSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.API_School;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/API_School/ApiSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API_School;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
